d61c4c6f6bad4bc5d0f1fdd7735e5b118872a71b
Aakash Soni <inos92@gmail.com>
2023-04-17 19:41:30 -0500

    updated Content to include props and started updating products-index. hard-coded product examples in Content

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Header() {
   return (
     <header>
@@ -29,7 +31,21 @@ function ProductsNew() {
   );
 }
 
-function ProductsIndex() {}
+function ProductsIndex(props) {
+  console.log(props);
+  return (
+    <div id="products-index">
+      <h1>All Products</h1>
+      {props.products.map(product) => (
+        <div key={product.id} className="products">
+          <h2>{product.title}</h2>
+          
+        </div>
+      )
+      }
+    </div>
+  );
+}
 
 function Footer() {
   return (
@@ -40,10 +56,27 @@ function Footer() {
 }
 
 function Content() {
+  let products = [
+    {
+      id: 1,
+      name: "Jelly Belly",
+      description: "A soft bean-shaped candy filled with various flavors.",
+      price: "3",
+      image_url: "https://cdn-tp1.mozu.com/9046-11441/cms/11441/files/1717d1ab-8e9f-4370-8eb6-e34dce41b1ea?max=650",
+    },
+    {
+      id: 2,
+      name: "Hershey's Kisses",
+      description: "A small droplet-shaped chocolate candy.",
+      price: "5",
+      image_url:
+        "https://images.heb.com/is/image/HEBGrocery/001970187?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0",
+    },
+  ];
   return (
     <div>
       <ProductsNew />
-      <ProductsIndex />
+      <ProductsIndex products={products} />
     </div>
   );
 }
